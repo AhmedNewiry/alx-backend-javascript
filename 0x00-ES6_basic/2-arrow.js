9bfb8fac5c4dad38ed93b64e5e38a7a4341c035f
AhmedNewiry <ahmedabdalfttah@outlook.com>
2024-07-22 19:39:04 -0700

    fix

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,6 +1,5 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-
   // Arrow function for addNeighborhood
   this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
