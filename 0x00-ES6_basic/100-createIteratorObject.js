09c4f12987415edb174aa7b57a9a016190581dc1
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-07-23 10:41:28 +0000

    fix

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,10 +1,20 @@
-export default function* createIteratorObject(report) {
-  const allEmployees = report.allEmployees;
-  for (const department in allEmployees) {
-    if (allEmployees.hasOwnProperty(department)) {
-      for (const employee of allEmployees[department]) {
-        yield employee;
-      }
-    }
+export default function createIteratorObject(report) {
+  let allEmployees = [];
+  for (const department of Object.values(report.allEmployees)) {
+    allEmployees = allEmployees.concat(department);
   }
+
+  return {
+    [Symbol.iterator]() {
+      let index = 0;
+      return {
+        next() {
+          if (index < allEmployees.length) {
+            return { value: allEmployees[index++], done: false };
+          }
+          return { done: true };
+        },
+      };
+    },
+  };
 }
