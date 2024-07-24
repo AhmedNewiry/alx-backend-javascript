775dc9bf96c5a110b5f398f106ce7f21b8e3141e
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-07-24 10:50:11 +0000

    fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,14 +4,11 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
+    uploadPhoto(fileName),
   ]).then(results => {
-    return results.map(result => {
-      if (result.status === 'fulfilled') {
-        return { status: result.status, value: result.value };
-      } else {
-        return { status: result.status, value: result.reason.message };
-      }
-    });
+    return results.map(result => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason.message
+    }));
   });
 }
