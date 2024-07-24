da967674b4e5f2702b37356712ab02950c1422a7
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-07-24 06:00:31 +0000

    fix'

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
 getFullResponseFromAPI(success) {
-	return new Promise((resolve, reject) => {
-		if (success) {
-			resolve({status:200, body:'Success'})
-		}esle {
-			reject(new Error("The fake API is not working currently"))
-		}
-	})
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({status:200, body:'Success'})
+    } esle {
+      reject(new Error("The fake API is not working currently"))
+    }
+  })
 }
