027d9baaee6614a87ce7bfc29cabdd7be0cf0f9a
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-07-25 14:38:25 +0000

    fix

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -32,7 +32,7 @@ export default class HolbertonCourse {
   }
 
   set students(value) {
-    if (!Array.isArray(value) || !value.every((studen)t => typeof student === 'string')) {
+    if (!Array.isArray(value) || !value.every((studen) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
        }
     this._students = value;
