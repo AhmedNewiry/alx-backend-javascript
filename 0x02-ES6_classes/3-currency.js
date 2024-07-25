4cd0c73592b66471f09639325b56bfee3cca25c6
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-07-25 20:14:40 +0000

    fix

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -14,19 +14,19 @@ export default class Currency {
     }
     this._code = value;
   }
-  
+
   get name() {
      return this._name;
   }
-  
+
   set name(value) {
     if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = value;
-   }
+  }
 
-   displayFullCurrency() {
+  displayFullCurrency() {
      return `${this._name} (${this._code})`;
-   }
+  }
 }
