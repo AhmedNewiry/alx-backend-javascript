84816944dba5a8e2d4aea14fecca738e353d6528
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-07-30 21:41:32 +0000

    weak

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,13 @@
+const weakMap = new WeakMap();
+function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  let count = weakMap.get(endpoint);
+  count += 1;
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+  weakMap.set(endpoint, count);
+}
+export { weakMap, queryAPI };
