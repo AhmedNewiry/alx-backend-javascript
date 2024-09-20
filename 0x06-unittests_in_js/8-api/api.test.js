aa9cd8acb360d65e65b2c41e127614102fc271c3
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-09-20 23:35:44 +0000

    8api

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,27 +2,19 @@ const request = require('request');
 const { expect } = require('chai');
 const app = require('./api');
 
-let server;
+describe('Index page', () => {
+  const serverUrl = 'http://localhost:7865';
 
-describe('API integration test', () => {
-  const API_URL = 'http://localhost:7865';
-
-  before((done) => {
-    server = app.listen(7865, () => {
+  it('should return status 200', (done) => {
+    request.get(serverUrl, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
       done();
     });
   });
 
-  after((done) => {
-    server.close(() => {
-      done();
-    });
-  });
-
-  it('GET / returns correct response', (done) => {
-    request.get(`${API_URL}/`, (_err, res, body) => {
-      expect(res.statusCode).to.be.equal(200);
-      expect(body).to.be.equal('Welcome to the payment system');
+  it('should return the correct message', (done) => {
+    request.get(serverUrl, (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
       done();
     });
   });
