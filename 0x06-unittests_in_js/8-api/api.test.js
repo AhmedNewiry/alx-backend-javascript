ef67f2a1147901ef5e8888908ccb0411657060da
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-09-20 15:36:43 +0000

    api test

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,24 +2,30 @@ const request = require('request');
 const { expect } = require('chai');
 const app = require('./api');
 
-describe('Index page', () => {
-  const url = 'http://localhost:7865';
+let server;
 
+describe('Index page', () => {
   before((done) => {
-    app.listen(7865, () => {
+    server = app.listen(7865, () => {
+      done();
+    });
+  });
+
+  after((done) => {
+    server.close(() => {
       done();
     });
   });
 
   it('should return status code 200', (done) => {
-    request(url, (error, response, body) => {
+    request('http://localhost:7865', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
       done();
     });
   });
 
   it('should return the correct message', (done) => {
-    request(url, (error, response, body) => {
+    request('http://localhost:7865', (error, response, body) => {
       expect(body).to.equal('Welcome to the payment system');
       done();
     });
