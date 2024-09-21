d2855ca927b655eec7658d6b623d58e04c9a27c0
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-09-21 00:56:37 +0000

    payment test

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,29 +1,23 @@
 const sinon = require('sinon');
-const { expect } = require('chai');
 const Utils = require('./utils');
-const { sendPaymentRequestToApi } = require('./3-payment');
+const sendPaymentRequestToApi = require('./3-payment');
+const { expect } = require('chai');
 
-describe('sendPaymentRequestToApi', () => {
+describe('sendPaymentRequestToApi', function () {
   let spy;
 
-  before(() => {
+  beforeEach(function () {
     spy = sinon.spy(Utils, 'calculateNumber');
   });
 
-  after(() => {
+  afterEach(function () {
     spy.restore();
   });
 
-  it('should call calculateNumber with the correct arguments', () => {
+  it('should call Utils.calculateNumber with SUM, 100, and 20', function () {
     sendPaymentRequestToApi(100, 20);
+
     expect(spy.calledOnce).to.be.true;
     expect(spy.calledWith('SUM', 100, 20)).to.be.true;
-  });
-
-  it('should log the correct total', () => {
-    const consoleLog = sinon.stub(console, 'log');
-    sendPaymentRequestToApi(100, 20);
-    expect(consoleLog.calledWith('The total is: 120')).to.be.true;
-    consoleLog.restore();
   });
 });
